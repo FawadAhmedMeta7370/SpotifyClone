c3774d72dbbbbf52cdce995602e520b0544cf74b
FawadAhmedMeta7370 <fawad.ahmed@metafroliclabs.com>
2024-09-09 06:23:06 -0700

    First Commit

---
+++ src/Screens/Auth/Login/Login.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+import { Text, View } from "react-native";
+import { ILogin } from "../../../Constants/Interface";
+
+const Login : FC <ILogin> = () => {
+    return(
+        <View>
+            <Text>Spotify Clone</Text>
+        </View>
+    )
+}
+
+export default Login
